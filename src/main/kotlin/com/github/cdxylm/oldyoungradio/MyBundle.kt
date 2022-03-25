bec1b012da02b0d4483ae7417d475cb31898ebf6
GitHub Action <action@github.com>
2022-03-25 13:53:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/cdxylm/oldyoungradio/MyBundle.kt
+++ src/main/kotlin/com/github/cdxylm/oldyoungradio/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cdxylm.oldyoungradio
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cdxylm/oldyoungradio/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cdxylm/oldyoungradio/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cdxylm.oldyoungradio.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cdxylm.oldyoungradio.MyBundle
 
 class MyApplicationService {
 
