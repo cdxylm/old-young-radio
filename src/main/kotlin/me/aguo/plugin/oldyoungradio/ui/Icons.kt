c7feaa7b62b6a03f5f0caedb42e9141045a49578
aguo <i@aguo.me>
2022-04-16 23:24:41 +0800

    v0.0.3

--- src/main/kotlin/me/aguo/plugin/oldyoungradio/ui/Icons.kt
+++ src/main/kotlin/me/aguo/plugin/oldyoungradio/ui/Icons.kt
@@ -5,16 +5,8 @@ import com.intellij.openapi.util.IconLoader
 object PluginIcons {
     @JvmField
     val mainIcon = IconLoader.getIcon("/icons/bili.svg", javaClass)
+    val mainIconGray = IconLoader.getIcon("/icons/bili_gray.svg", javaClass)
     val stopIcon = IconLoader.getIcon("/icons/player_stop.svg", javaClass)
     val onlineIcon = IconLoader.getIcon("/icons/online.svg", javaClass)
     val offlineIcon = IconLoader.getIcon("/icons/offline.svg", javaClass)
-}
-
-object CatIcons {
-    @JvmField
-    val darkCatIcon_0 = IconLoader.getIcon("/icons/dark_cat_0.png", javaClass)
-    val darkCatIcon_1 = IconLoader.getIcon("/icons/dark_cat_1.png", javaClass)
-    val darkCatIcon_2 = IconLoader.getIcon("/icons/dark_cat_2.png", javaClass)
-    val darkCatIcon_3 = IconLoader.getIcon("/icons/dark_cat_3.png", javaClass)
-    val darkCatIcon_4 = IconLoader.getIcon("/icons/dark_cat_4.png", javaClass)
 }
